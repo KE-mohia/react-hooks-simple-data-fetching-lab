5f58a63ffe9348fe200060a24a83d988d7f154dc
wayne <keyawayne1@gmail.com>
2023-07-23 15:57:55 +0300

    assinment done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  const [dogImageUrl, setDogImageUrl] = useState(null);
+
+  useEffect(() => {
+    // Fetch data from the API
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => response.json())
+      .then((data) => setDogImageUrl(data.message))
+      .catch((error) => console.error('Error fetching dog image:', error));
+  }, []); // The empty dependency array ensures this effect runs only once when the component mounts
+
+  return (
+    <div>
+      {!dogImageUrl ? (
+        <p>Loading...</p>
+      ) : (
+        <img src={dogImageUrl} alt="A Random Dog" />
+      )}
+    </div>
+  );
+};
+
+export default App;
+
